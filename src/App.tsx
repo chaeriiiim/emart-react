07a0d92e05afaf961b2ac26c662a2b89b10f1daa
김채림 <woqja0905@naver.com>
2025-05-09 13:04:35 +0900

    del: 파일 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,11 @@
-import { Container } from "./components/Container";
-import { Root } from "./components/Root";
 import { GlobalStyle } from "./styles/GlobalStyle";
 
 function App() {
   return (
-    <Root>
+    <>
       <GlobalStyle />
-      <header>
-        <Container></Container>
-      </header>
-      <main>
-        <Container>
-          <h1>test</h1>
-        </Container>
-      </main>
-    </Root>
+      <h1>hi!</h1>
+    </>
   );
 }
 
