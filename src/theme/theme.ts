55875b41b0ac012467e0c6b39b6eff89ce57a2b1
김채림 <woqja0905@naver.com>
2025-05-09 13:49:44 +0900

    update:color 업로드

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -9,8 +9,27 @@ const fontSize = {
   xxl: "1.75rem", // 28px
 };
 
+const color = {
+  mainYellow: "#ffd040",
+  blue: "#0000ee",
+  black: "#000000",
+  secondary: "#222222",
+  white: "#ffffff",
+  gray100: "#fafafa",
+  gray150: "#f5f5f5",
+  gray200: "#f0f0f0",
+  gray300: "#e5e5e5",
+  gray350: "#cfcfcf",
+  gray400: "#969696",
+  gray500: "#888888",
+  gray600: "#777777",
+  gray700: "#666666",
+  gray800: "#444444",
+  gray900: "#222222",
+};
 const theme = {
   fontSize,
+  color,
 };
 
 export default theme;
