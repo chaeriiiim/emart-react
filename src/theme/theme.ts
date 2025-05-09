f447042799000d74ea4b81b1bfd20e22149005fd
김채림 <woqja0905@naver.com>
2025-05-09 13:39:01 +0900

    update : font-size

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,6 +1,12 @@
 const fontSize = {
-  xs: "",
-  base: "1rem", // 16px
+  xxs: "0.625rem", // 10px
+  xs: "0.75rem", // 12px
+  sm: "0.8125rem", // 13px
+  base: "0.875rem", // 14px
+  md: "1rem", // 16px
+  lg: "1.25rem", // 20px
+  xl: "1.5rem", // 24px
+  xxl: "1.75rem", // 28px
 };
 
 const theme = {
