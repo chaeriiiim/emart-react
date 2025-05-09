ce84be69f04a4873406e7a0a783dc6cb65a910eb
김채림 <woqja0905@naver.com>
2025-05-09 12:32:35 +0900

    update : 태그 css 초기화

---
+++ src/styles/GlobalStyle.ts
@@ -0,0 +1,34 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  /* 기본 초기화 */
+  * {
+    margin:0;
+    padding:0;
+    box-sizing:border-box;
+  }
+
+  html, body {
+    width:100%;
+    height :100%;
+    font-family:'Noto Sans KR', 'sans-serif;
+    background-color:#fff;
+    color:#000;
+  }
+
+  a{
+    text-decoration:none;
+    color:inherit;
+  }
+
+  ul, li{
+    list-style:none;
+  }
+
+  button {
+    border : none;
+    background:none;
+    cursor : pointer;
+    font-family:inherit;
+  }
+`;
