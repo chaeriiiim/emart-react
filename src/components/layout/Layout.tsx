3c7cddc40718e1aaf11560b53b367752f69c5df0
김채림 <woqja0905@naver.com>
2025-05-09 16:41:22 +0900

    update : Root, Container 오류로 Layout에서 따로 css 적용

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,10 +1,48 @@
-import { Outlet } from "react-router-dom";
-import Container from "../common/Container";
-import Root from "../common/Root";
+// import { Outlet } from "react-router-dom";
+// import Container from "../common/Container";
+// import Root from "../common/Root";
 import Footer from "../layout/Footer";
 import Header from "../layout/Header";
 
-function Layout() {
+// function Layout() {
+//   return (
+//     <Root style={{ width: "100%", height: "100vh" }}>
+//       {/* <Header /> */}
+//       <h1>header!</h1>
+//       <Container
+//         style={{
+//           width: "1280px",
+//           height: "500px",
+//           margin: "0 auto",
+//           border: "1px solid red",
+//         }}
+//       >
+//         <Outlet />
+//       </Container>
+//       {/* <Footer /> */}
+//     </Root>
+//   );
+// }
+
+// export default Layout;
+
+import { Outlet } from "react-router-dom";
+import styled from "styled-components";
+
+const Root = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  background-color: #fafafa;
+`;
+
+const Container = styled.div`
+  width: 1280px;
+  margin: 0 auto;
+  border: 2px dashed red;
+  padding: 2rem;
+`;
+
+export default function Layout() {
   return (
     <Root>
       <Header />
@@ -15,5 +53,3 @@ function Layout() {
     </Root>
   );
 }
-
-export default Layout;
