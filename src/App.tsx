4f7572c47ee2d4b271878a06be392ab459ad3d15
김채림 <woqja0905@naver.com>
2025-05-09 14:17:27 +0900

    update : Root 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
+import Root from "./components/Root";
 import { GlobalStyle } from "./styles/GlobalStyle";
 
 function App() {
   return (
-    <>
+    <Root>
       <GlobalStyle />
       <h1>hi!</h1>
-    </>
+    </Root>
   );
 }
 
