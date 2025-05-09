07b6e5a603bb23684e4b06e72eb399c26cffe85c
김채림 <woqja0905@naver.com>
2025-05-09 14:03:27 +0900

    update : theme 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { ThemeProvider } from "styled-components";
 import App from "./App.tsx";
+import { GlobalStyle } from "./styles/GlobalStyle.ts";
+import theme from "./theme/theme.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </StrictMode>
 );
