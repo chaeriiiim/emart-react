ecd73108da7c65175426569fd570ecfe63220b01
김채림 <woqja0905@naver.com>
2025-05-12 15:09:55 +0900

    update : 위치수정

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,6 +1,14 @@
+import { useState } from "react";
 import Logo from "../../assets/e-mart_logo.svg";
 
 export default function Header() {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+    console.log(e.target.value);
+  };
+
   return (
     <div
       style={{
@@ -12,41 +20,54 @@ export default function Header() {
       }}
     >
       <div style={{ width: "1280px", height: "100%", border: "1px solid red" }}>
-        <img src={Logo} style={{ width: "160px", height: "56px" }}></img>
-        <div style={{ position: "relative", width: "320px" }}>
-          <input
-            type="text"
-            placeholder="AD) 켈로그 첵스 신제품 출시"
-            style={{
-              borderRadius: "20px",
-              border: "none",
-              outline: "none",
-              backgroundColor: "#f5f5f5",
-              width: "320px",
-              height: "44px",
-              padding: "6px 52px 6px 16px",
-            }}
-          />
-          <button
-            style={{
-              position: "absolute",
-              right: "6px",
-              top: "50%",
-              transform: "translateY(-50%)",
-              width: "32px",
-              height: "32px",
-              borderRadius: "50%",
-              backgroundColor: "#000",
-              border: "none",
-              color: "#fff",
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-              cursor: "pointer",
-            }}
-          >
-            버튼
-          </button>
+        <div
+          style={{
+            width: "1280px",
+            height: "100%",
+            border: "1px solid red",
+            display: "flex",
+            alignItems: "center",
+            gap: "24px",
+          }}
+        >
+          <img src={Logo} style={{ width: "160px", height: "56px" }}></img>
+          <div style={{ position: "relative", width: "320px" }}>
+            <input
+              type="text"
+              placeholder="AD) 켈로그 첵스 신제품 출시"
+              onChange={handleChange}
+              value={inputValue}
+              style={{
+                borderRadius: "20px",
+                border: "none",
+                outline: "none",
+                backgroundColor: "#f5f5f5",
+                width: "320px",
+                height: "44px",
+                padding: "6px 52px 6px 16px",
+              }}
+            />
+            <button
+              style={{
+                position: "absolute",
+                right: "6px",
+                top: "50%",
+                transform: "translateY(-50%)",
+                width: "32px",
+                height: "32px",
+                borderRadius: "50%",
+                backgroundColor: "#000",
+                border: "none",
+                color: "#fff",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                cursor: "pointer",
+              }}
+            >
+              버튼
+            </button>
+          </div>
         </div>
       </div>
     </div>
