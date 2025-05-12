d211f46b630d81169d97ffb6de785bbbb3d42c51
김채림 <woqja0905@naver.com>
2025-05-12 12:02:57 +0900

    update : header 작업

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,14 +1,17 @@
-import styled from "styled-components";
-
-const Test = styled.div`
-  font-size: 50px;
-  color: red;
-`;
-
 export default function Header() {
   return (
-    <div>
-      <Test>test!</Test>
+    <div
+      style={{
+        width: "100%",
+        height: "72px",
+        backgroundColor: "#ffd040",
+        display: "flex",
+        justifyContent: "center",
+      }}
+    >
+      <div style={{ width: "1280px", height: "100%", border: "1px solid red" }}>
+        <img src=""></img>
+      </div>
     </div>
   );
 }
