ffa6e8b30e4ce55007471a9c64f280aa99a2a4cb
김채림 <woqja0905@naver.com>
2025-05-12 21:56:00 +0900

    update : css 수정

---
+++ src/components/layout/HeaderNav.tsx
@@ -0,0 +1,7 @@
+// export default function HeaderNav() {
+//   return (
+//     <div style={{ width: "100%", height: "42px" }}>
+//       <h1>test!</h1>
+//     </div>
+//   );
+// }
--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,30 +1,5 @@
-// import { Outlet } from "react-router-dom";
-// import Container from "../common/Container";
-// import Root from "../common/Root";
 import Footer from "../layout/Footer";
 import Header from "../layout/Header";
-
-// function Layout() {
-//   return (
-//     <Root style={{ width: "100%", height: "100vh" }}>
-//       {/* <Header /> */}
-//       <h1>header!</h1>
-//       <Container
-//         style={{
-//           width: "1280px",
-//           height: "500px",
-//           margin: "0 auto",
-//           border: "1px solid red",
-//         }}
-//       >
-//         <Outlet />
-//       </Container>
-//       {/* <Footer /> */}
-//     </Root>
-//   );
-// }
-
-// export default Layout;
 
 import { Outlet } from "react-router-dom";
 import styled from "styled-components";
@@ -45,6 +20,7 @@ const Container = styled.div`
 export default function Layout() {
   return (
     <Root>
+      {/* /<HeaderNav /> */}
       <Header />
       <Container>
         <Outlet />
