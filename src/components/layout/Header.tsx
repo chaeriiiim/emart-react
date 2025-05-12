eca0d19237e4482f9ccde3fdfec0a9586f1fb297
김채림 <woqja0905@naver.com>
2025-05-12 21:38:24 +0900

    update : styled-component 작업

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -25,6 +25,49 @@ const HeaderInnerWrapper = styled.div`
   gap: 24px;
 `;
 
+const LogoImg = styled.img`
+  width: 160px;
+  height: 56px;
+`;
+
+const InputWrapper = styled.div`
+  position: relative;
+  width: 320px;
+`;
+
+const SearchInput = styled.input`
+  border-radius: 20px;
+  border: none;
+  outline: none;
+  background-color: #f5f5f5;
+  width: 320px;
+  height: 44px;
+  padding: 6px 52px 6px 16px;
+`;
+
+const SearchButton = styled.button`
+  position: absolute;
+  right: 6px;
+  top: 50%;
+  transform: translateY(-50%);
+  width: 32px;
+  height: 32px;
+  border-radius: 50%;
+  background-color: #000;
+  border: none;
+  color: #fff;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+`;
+
+const HeaderOnClickWrapper = styled.div`
+  flexgrow: 1;
+  display: flex;
+  justify-content: flex-end;
+`;
+
 export default function Header() {
   const [inputValue, setInputValue] = useState("");
 
@@ -36,50 +79,20 @@ export default function Header() {
   return (
     <Root>
       <HeaderWrapper>
-        <HeaderInnerWrapper style={{}}>
-          <img src={Logo} style={{ width: "160px", height: "56px" }} />
-          <div style={{ position: "relative", width: "320px" }}>
-            <input
+        <HeaderInnerWrapper>
+          <LogoImg src={Logo} />
+          <InputWrapper>
+            <SearchInput
               type="text"
               placeholder="AD) 켈로그 첵스 신제품 출시"
               onChange={handleChange}
               value={inputValue}
-              style={{
-                borderRadius: "20px",
-                border: "none",
-                outline: "none",
-                backgroundColor: "#f5f5f5",
-                width: "320px",
-                height: "44px",
-                padding: "6px 52px 6px 16px",
-              }}
             />
-            <button
-              style={{
-                position: "absolute",
-                right: "6px",
-                top: "50%",
-                transform: "translateY(-50%)",
-                width: "32px",
-                height: "32px",
-                borderRadius: "50%",
-                backgroundColor: "#000",
-                border: "none",
-                color: "#fff",
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-                cursor: "pointer",
-              }}
-            >
-              버튼
-            </button>
-          </div>
-          <div
-            style={{ flexGrow: 1, display: "flex", justifyContent: "flex-end" }}
-          >
+            <SearchButton>버튼</SearchButton>
+          </InputWrapper>
+          <HeaderOnClickWrapper>
             <HeaderOnClick />
-          </div>
+          </HeaderOnClickWrapper>
         </HeaderInnerWrapper>
       </HeaderWrapper>
     </Root>
