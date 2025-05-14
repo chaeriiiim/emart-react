6a86a1b9101d8495fe582479f3bb83e186763cf3
김채림 <woqja0905@naver.com>
2025-05-14 12:25:44 +0900

    update: tsx 수정

--- src/components/layout/HeaderNav.tsx
+++ src/components/layout/HeaderNav.tsx
@@ -1,6 +1,35 @@
+import IconCart from "../../assets/navIcon_cart.svg";
+import IconEarphone from "../../assets/navIcon_earphone.svg";
+import IconHeart from "../../assets/navIcon_heart.svg";
+import IconHuman from "../../assets/navIcon_human.svg";
+import IconSearch from "../../assets/navIcon_search.svg";
 import SSGLogo from "../../assets/ssg_logo.svg";
 
 import styled from "styled-components";
+
+interface IconItem {
+  src: string;
+  alt: string;
+}
+
+const icons: IconItem[] = [
+  { src: IconSearch, alt: "검색 아이콘" },
+  { src: IconHeart, alt: "좋아요 아이콘" },
+  { src: IconHuman, alt: "좋아요 아이콘" },
+  { src: IconCart, alt: "좋아요 아이콘" },
+  { src: IconEarphone, alt: "좋아요 아이콘" },
+];
+
+interface NavItem {
+  href: string;
+  name: string;
+}
+
+const nav: NavItem[] = [
+  { href: "#", name: "이마트몰" },
+  { href: "#", name: "신세계몰" },
+  { href: "#", name: "신세계백화점" },
+];
 
 const NavWrapper = styled.div`
   width: 1280px;
@@ -8,6 +37,9 @@ const NavWrapper = styled.div`
   margin: 0 auto;
   display: flex;
   align-items: center;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 `;
 
 const Logo = styled.img`
@@ -28,40 +60,63 @@ const NavItem = styled.li`
 
   &:not(:last-child)::after {
     content: "|";
-    position: block;
+    display: inline-block;
     right: 0;
     color: #e5e5e5;
-    // padding-left: 12px;
     margin-left: 8px;
+  }
+
+  &:nth-child(1) a {
+    font-weight: bold;
+    color: #000;
+  }
+
+  &:nth-child(2) a {
+    color: #666666;
+  }
+
+  &:nth-child(3) a {
+    color: #666666;
   }
 
   a {
     text-decoration: none;
     color: inherit;
     font-weight: normal;
-
-    &:first-child {
-      font-weight: bold;
-      color: #000;
-    }
   }
+`;
+
+const IconWrapper = styled.div`
+  & img {
+    width: 20px;
+    height: 20px;
+    margin: 0 0 0 5px;
+  }
+`;
+
+const NavLeftGroup = styled.div`
+  display: flex;
+  align-items: center;
 `;
 
 export default function HeaderNav() {
   return (
     <NavWrapper>
-      <Logo src={SSGLogo} alt="ssg 로고" />
-      <NavList>
-        <NavItem>
-          <a href="#">이마트몰</a>
-        </NavItem>
-        <NavItem>
-          <a href="#">신세계몰</a>
-        </NavItem>
-        <NavItem>
-          <a href="#">신세계백화점</a>
-        </NavItem>
-      </NavList>
+      <NavLeftGroup>
+        <Logo src={SSGLogo} alt="ssg 로고" />
+        <NavList>
+          {nav.map((item, index) => (
+            <NavItem key={index}>
+              <a href={item.href}>{item.name}</a>
+            </NavItem>
+          ))}
+        </NavList>
+      </NavLeftGroup>
+      <IconWrapper>
+        {icons.map((icon, index) => (
+          <img key={index} src={icon.src} alt={icon.alt} />
+        ))}
+      </IconWrapper>
     </NavWrapper>
   );
 }
