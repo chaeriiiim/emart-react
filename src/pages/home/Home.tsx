29f510422372f2c1104aa4cf3d07cae168f999f4
김채림 <woqja0905@naver.com>
2025-05-14 14:14:41 +0900

    update : import 페이지 추가

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,10 +1,12 @@
 import Banner from "../../components/common/Banner";
+import DeliverySection from "./DeliverySection";
 import HomeSlider from "./HomeSlider";
 
 export default function Home() {
   return (
     <div>
       <HomeSlider />
+      <DeliverySection />
       <Banner />
     </div>
   );
