9814590f630ddab9ed6f61ccf0126bb46cffb3ed
김채림 <woqja0905@naver.com>
2025-05-14 14:09:56 +0900

    add : 파일생성

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,43 +1,48 @@
+import Banner from "../../components/common/Banner";
+
 export default function Home() {
   return (
-    <div
-      style={{
-        width: "100%",
-        height: "500px",
-        border: "1px solid red",
-        display: "flex",
-        gap: "24px",
-      }}
-    >
-      <div>
-        <div
-          style={{
-            width: "837px",
-            height: "500px",
-            backgroundColor: "gray",
-            borderRadius: "10px",
-          }}
-        ></div>
-      </div>
+    <div>
+      <div
+        style={{
+          width: "100%",
+          height: "500px",
+          border: "1px solid red",
+          display: "flex",
+          gap: "24px",
+        }}
+      >
+        <div>
+          <div
+            style={{
+              width: "837px",
+              height: "500px",
+              backgroundColor: "gray",
+              borderRadius: "10px",
+            }}
+          ></div>
+        </div>
 
-      <div style={{ display: "flex", flexDirection: "column", gap: "24px" }}>
-        <div
-          style={{
-            width: "419px",
-            height: "238px",
-            backgroundColor: "gray",
-            borderRadius: "10px",
-          }}
-        ></div>
-        <div
-          style={{
-            width: "419px",
-            height: "238px",
-            backgroundColor: "gray",
-            borderRadius: "10px",
-          }}
-        ></div>
+        <div style={{ display: "flex", flexDirection: "column", gap: "24px" }}>
+          <div
+            style={{
+              width: "419px",
+              height: "238px",
+              backgroundColor: "gray",
+              borderRadius: "10px",
+            }}
+          ></div>
+          <div
+            style={{
+              width: "419px",
+              height: "238px",
+              backgroundColor: "gray",
+              borderRadius: "10px",
+            }}
+          ></div>
+        </div>
       </div>
+      <Banner />
     </div>
   );
 }
