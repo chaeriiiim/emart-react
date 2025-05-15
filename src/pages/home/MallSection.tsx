6d2d3c1bcb59e43e4f75195d67d01445801ee3b4
김채림 <woqja0905@naver.com>
2025-05-15 15:01:02 +0900

    update : mall section 에 tab 구현

--- src/pages/home/MallSection.tsx
+++ src/pages/home/MallSection.tsx
@@ -1,5 +1,13 @@
 import Tap from "../../components/common/Tap";
 
+const tabsData = [
+  { id: "banner_1", label: "오반장" },
+  { id: "banner_2", label: "1+1" },
+  { id: "banner_3", label: "마감세일" },
+  { id: "banner_4", label: "이마트 전단행사" },
+  { id: "banner_5", label: "트레이더스 점포행사" },
+];
+
 export default function MallSection() {
   return (
     <div
@@ -7,9 +15,10 @@ export default function MallSection() {
         width: "100%",
         height: "300px",
         marginTop: "120px",
+        border: "1px solid red",
       }}
     >
-      <Tap />
+      <Tap tabs={tabsData} />
     </div>
   );
 }
