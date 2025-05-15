387be6ad032071cd6a3d7b8cd7d78beadaa2d93c
김채림 <woqja0905@naver.com>
2025-05-15 12:22:44 +0900

    update : banner 작업

--- src/components/common/Banner.tsx
+++ src/components/common/Banner.tsx
@@ -1,16 +1,68 @@
 import BannerImg from "../../assets/banner_1.webp";
 
+// export default function Banner() {
+//   return (
+//     <div
+//       style={{
+//         width: "100%",
+//         height: "120px",
+//         marginTop: "120px",
+//       }}
+//     >
+//       <a
+//         href="#"
+//         style={{
+//           width: "1280px",
+//           height: "120px",
+//           backgroundImage: `url(${BannerImg})`,
+//           backgroundSize: "cover",
+//           backgroundPosition: "center",
+//           position: "relative",
+//         }}
+//       >
+//         <div>
+//           <span>지금 SSG.COM Biz. 가입하고, 혜택 받으세요!</span>
+//         </div>
+//       </a>
+//     </div>
+//   );
+// }
+
 export default function Banner() {
   return (
     <div
       style={{
         width: "100%",
         height: "120px",
-        backgroundColor: "black",
         marginTop: "120px",
       }}
     >
-      <img src={BannerImg} style={{ width: "1280px", height: "120px" }} />
+      <a href="#">
+        <div
+          style={{
+            width: "1280px",
+            height: "120px",
+            backgroundImage: `url(${BannerImg})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            position: "relative",
+          }}
+        >
+          <span
+            style={{
+              position: "absolute",
+              // left: "32px",
+              // top: "50%",
+              // transform: "translateY(-50%)",
+              // color: "#fff",
+              // fontWeight: "bold",
+              // fontSize: "16px",
+            }}
+          >
+            지금 SSG.COM Biz. 가입하고, 혜택 받으세요!
+          </span>
+        </div>
+      </a>
     </div>
   );
 }
