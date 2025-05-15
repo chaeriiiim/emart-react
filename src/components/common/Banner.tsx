6f5c8a139b6c35ddddbecbfe9df5e941d491d1a1
김채림 <woqja0905@naver.com>
2025-05-15 12:28:20 +0900

    update : 주석 처리

--- src/components/common/Banner.tsx
+++ src/components/common/Banner.tsx
@@ -7,33 +7,29 @@ export default function Banner() {
         width: "100%",
         height: "120px",
         marginTop: "120px",
+        backgroundImage: `url(${BannerImg})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        position: "relative",
       }}
     >
+      {/* props 적용 시 backgroundImage 각각 다른 값 */}
       <a href="#">
-        <div
+        {/* props 적용 시 href 각각 다른 값 */}
+        <span
           style={{
-            width: "1280px",
-            height: "120px",
-            backgroundImage: `url(${BannerImg})`,
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-            position: "relative",
+            position: "absolute",
+            // left: "32px",
+            // top: "50%",
+            // transform: "translateY(-50%)",
+            // color: "#fff",
+            // fontWeight: "bold",
+            // fontSize: "16px",
           }}
         >
-          <span
-            style={{
-              position: "absolute",
-              // left: "32px",
-              // top: "50%",
-              // transform: "translateY(-50%)",
-              // color: "#fff",
-              // fontWeight: "bold",
-              // fontSize: "16px",
-            }}
-          >
-            지금 SSG.COM Biz. 가입하고, 혜택 받으세요!
-          </span>
-        </div>
+          지금 SSG.COM Biz. 가입하고, 혜택 받으세요!
+          {/* props 적용 시 contents 각각 다른 값(css에서 color 도 다른 값) */}
+        </span>
       </a>
     </div>
   );
