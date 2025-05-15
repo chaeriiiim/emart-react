afc96debba4067eec491ddca4c5ac02b7904c1d5
김채림 <woqja0905@naver.com>
2025-05-15 14:31:10 +0900

    update : 클릭 이벤트 추가

--- src/components/common/Tap.tsx
+++ src/components/common/Tap.tsx
@@ -1,79 +1,58 @@
+import { useState } from "react";
+
+import styled from "styled-components";
+
+interface tabsItem {
+  id: string;
+  label: string;
+}
+
+const tabs: tabsItem[] = [
+  { id: "banner_1", label: "오반장" },
+  { id: "banner_2", label: "1+1" },
+  { id: "banner_3", label: "마감세일" },
+  { id: "banner_4", label: "이마트 전단행사" },
+  { id: "banner_5", label: "트레이더스 점포행사" },
+];
+
+const TapWrapper = styled.div`
+  width: 100%;
+  height: 56px;
+  background-color: #f5f5f5;
+  border-radius: 12px;
+  display: flex;
+`;
+
+const TapInnerWrapper = styled.div<{ active: boolean }>`
+  width: 256px;
+  height: 56px;
+  background-color: ${({ active }) => (active ? "#FFD040" : "#F5F5F5")};
+  border-radius: 12px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  & p {
+    color: #777777;
+  }
+`;
+
 export default function Tap() {
+  const [activeTab, setActiveTab] = useState<string>("banner_1");
   return (
-    <div
-      style={{
-        width: "100%",
-        height: "56px",
-        backgroundColor: "#F5F5F5",
-        borderRadius: "12px",
-        display: "flex",
-      }}
-    >
-      <div
-        style={{
-          width: "256px",
-          height: "56px",
-          backgroundColor: "#FFD040",
-          borderRadius: "12px",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <p style={{ color: "#777777" }}>오반장</p>
-      </div>
-      <div
-        style={{
-          width: "256px",
-          height: "56px",
-          backgroundColor: "#F5F5F5",
-          borderRadius: "12px",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <p style={{ color: "#777777" }}>1+1</p>
-      </div>
-      <div
-        style={{
-          width: "256px",
-          height: "56px",
-          backgroundColor: "#F5F5F5",
-          borderRadius: "12px",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <p style={{ color: "#777777" }}>마감세일</p>
-      </div>
-      <div
-        style={{
-          width: "256px",
-          height: "56px",
-          backgroundColor: "#F5F5F5",
-          borderRadius: "12px",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <p style={{ color: "#777777" }}>이마트 전단행사</p>
-      </div>
-      <div
-        style={{
-          width: "256px",
-          height: "56px",
-          backgroundColor: "#F5F5F5",
-          borderRadius: "12px",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <p style={{ color: "#777777" }}>트레이더스 점포행사</p>
-      </div>
-    </div>
+    <TapWrapper>
+      {tabs.map((tab) => (
+        <TapInnerWrapper
+          key={tab.id}
+          onClick={(e) => {
+            e.preventDefault();
+            setActiveTab(tab.id);
+          }}
+          active={activeTab === tab.id}
+        >
+          <p>{tab.label}</p>
+        </TapInnerWrapper>
+      ))}
+    </TapWrapper>
   );
 }
