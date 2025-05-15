35ad0eee92aaa4d243ddb9186993f288840dab1e
김채림 <woqja0905@naver.com>
2025-05-15 15:36:13 +0900

    update : button contents 추가

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -2,6 +2,8 @@ export default function Button() {
   return (
     <button
       style={{ width: "480px", height: "60px", border: "1px solid #e5e5e5" }}
-    ></button>
+    >
+      오반장 전체보기
+    </button>
   );
 }
