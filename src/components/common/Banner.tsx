59666f8b8f07fc155d2ea4d32d5a4f4fb5e2b384
김채림 <woqja0905@naver.com>
2025-05-15 13:02:15 +0900

    updatae : props 적용 완성

--- src/components/common/Banner.tsx
+++ src/components/common/Banner.tsx
@@ -1,45 +1,58 @@
-import BannerImg from "../../assets/banner_1.webp";
+import styled from "styled-components";
 
-export default function Banner() {
+interface BannerProps {
+  backgroundImg: string;
+  href: string;
+  title: string;
+  description: string;
+  titleColor?: string;
+  descColor?: string;
+}
+
+const BannerWrapper = styled.div<{ backgroundImg: string }>`
+  width: 100%;
+  height: 120px;
+  margin-top: 120px;
+  background-image: url(${(props) => props.backgroundImg});
+  background-size: cover;
+  background-position: center;
+  position: relative;
+`;
+
+const BannerTextWrapper = styled.div`
+  position: absolute;
+  left: 32px;
+  top: 50%;
+  transform: translateY(-50%);
+`;
+
+const Title = styled.span`
+  color: #fff;
+  font-weight: bold;
+  font-size: 20px;
+`;
+
+const Description = styled.p`
+  color: #fff;
+  margin-top: 2px;
+`;
+
+export default function Banner({
+  backgroundImg,
+  href,
+  title,
+  titleColor,
+  description,
+  descColor,
+}: BannerProps) {
   return (
-    <div
-      style={{
-        width: "100%",
-        height: "120px",
-        marginTop: "120px",
-        backgroundImage: `url(${BannerImg})`,
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-        position: "relative",
-      }}
-    >
-      {/* props 적용 시 backgroundImage 각각 다른 값 */}
-      <a href="#">
-        {/* props 적용 시 href 각각 다른 값 */}
-        <div
-          style={{
-            position: "absolute",
-            left: "32px",
-            top: "50%",
-            transform: "translateY(-50%)",
-          }}
-        >
-          <span
-            style={{
-              color: "#fff",
-              fontWeight: "bold",
-              fontSize: "20px",
-            }}
-          >
-            지금 SSG.COM Biz. 가입하고, 혜택 받으세요!
-            {/* props 적용 시 contents 각각 다른 값(css에서 color 도 다른 값) */}
-          </span>
-          <p style={{ color: "#fff", marginTop: "2px" }}>
-            가입 즉시 10% 할인 쿠폰과 매월 6종 쿠폰 자동 지급까지
-            {/* props 적용 시 contents 각각 다른 값(css에서 color 도 다른 값) */}
-          </p>
-        </div>
+    <BannerWrapper backgroundImg={backgroundImg}>
+      <a href={href}>
+        <BannerTextWrapper>
+          <Title color={titleColor}>{title}</Title>
+          <Description color={descColor}>{description}</Description>
+        </BannerTextWrapper>
       </a>
-    </div>
+    </BannerWrapper>
   );
 }
