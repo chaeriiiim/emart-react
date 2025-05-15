10c499a09dfffcfa24bc505b72de297fa89fc9f6
김채림 <woqja0905@naver.com>
2025-05-15 13:04:20 +0900

    update : props 코드 적용

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,12 +2,21 @@ import Banner from "../../components/common/Banner";
 import DeliverySection from "./DeliverySection";
 import HomeSlider from "./HomeSlider";
 
+import BannerImg from "../../assets/banner_1.webp";
+
 export default function Home() {
   return (
     <div>
       <HomeSlider />
       <DeliverySection />
-      <Banner />
+      <Banner
+        backgroundImg={BannerImg}
+        href="#"
+        title="지금 SSG.COM Biz. 가입하고, 혜택 받으세요!"
+        description="가입 즉시 10% 할인 쿠폰과 매월 6종 쿠폰 자동 지급까지"
+        titleColor="#fff"
+        descColor="#fff"
+      />
     </div>
   );
 }
