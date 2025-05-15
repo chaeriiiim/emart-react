503e6f674efb38fc608ef059cedede9835daa19e
김채림 <woqja0905@naver.com>
2025-05-15 12:42:48 +0900

    update : 기본틀 완성

--- src/components/common/Banner.tsx
+++ src/components/common/Banner.tsx
@@ -16,20 +16,29 @@ export default function Banner() {
       {/* props 적용 시 backgroundImage 각각 다른 값 */}
       <a href="#">
         {/* props 적용 시 href 각각 다른 값 */}
-        <span
+        <div
           style={{
             position: "absolute",
-            // left: "32px",
-            // top: "50%",
-            // transform: "translateY(-50%)",
-            // color: "#fff",
-            // fontWeight: "bold",
-            // fontSize: "16px",
+            left: "32px",
+            top: "50%",
+            transform: "translateY(-50%)",
           }}
         >
-          지금 SSG.COM Biz. 가입하고, 혜택 받으세요!
-          {/* props 적용 시 contents 각각 다른 값(css에서 color 도 다른 값) */}
-        </span>
+          <span
+            style={{
+              color: "#fff",
+              fontWeight: "bold",
+              fontSize: "20px",
+            }}
+          >
+            지금 SSG.COM Biz. 가입하고, 혜택 받으세요!
+            {/* props 적용 시 contents 각각 다른 값(css에서 color 도 다른 값) */}
+          </span>
+          <p style={{ color: "#fff", marginTop: "2px" }}>
+            가입 즉시 10% 할인 쿠폰과 매월 6종 쿠폰 자동 지급까지
+            {/* props 적용 시 contents 각각 다른 값(css에서 color 도 다른 값) */}
+          </p>
+        </div>
       </a>
     </div>
   );
