c13944b13107db45b63de3d4671c7d8818a8ed88
김채림 <woqja0905@naver.com>
2025-05-15 13:07:21 +0900

    update : mall section 파일 생성

---
+++ src/pages/home/MallSection.tsx
@@ -0,0 +1,12 @@
+export default function MallSection() {
+  return (
+    <div
+      style={{
+        width: "100%",
+        height: "300px",
+        border: "1px solid red",
+        marginTop: "120px",
+      }}
+    ></div>
+  );
+}
