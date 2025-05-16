b23a86ad38978b510f0b61418e7aa1faab7894c1
김채림 <woqja0905@naver.com>
2025-05-16 11:49:07 +0900

    update : button styled 설정

--- src/components/common/HomePageButton.tsx
+++ src/components/common/HomePageButton.tsx
@@ -1,9 +1,23 @@
+import styled from "styled-components";
+
+const ButtonWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
 export default function HomePageButton() {
   return (
-    <button
-      style={{ width: "480px", height: "60px", border: "1px solid #e5e5e5" }}
-    >
-      오반장 전체보기
-    </button>
+    <ButtonWrapper>
+      <button
+        style={{
+          width: "480px",
+          height: "60px",
+          border: "1px solid #e5e5e5",
+          borderRadius: "8px",
+        }}
+      >
+        오반장 전체보기
+      </button>
+    </ButtonWrapper>
   );
 }
