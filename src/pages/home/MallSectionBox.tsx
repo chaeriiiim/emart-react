cb3035eb90c08d3b6e0d6df5e3157075a591a50c
김채림 <woqja0905@naver.com>
2025-05-19 13:29:23 +0900

    update : product 레이아웃 작업 중

--- src/pages/home/MallSectionBox.tsx
+++ src/pages/home/MallSectionBox.tsx
@@ -1,7 +1,22 @@
+import ProductImg from "../../assets/product_1.avif";
+
 export default function MallSectionBox() {
   return (
     <div
-      style={{ width: "302px", height: "302px", border: "1px solid black" }}
-    ></div>
+      style={{
+        width: "302px",
+        height: "302px",
+        gap: "0 24px",
+        display: "flex",
+      }}
+    >
+      <div>
+        <img
+          src={ProductImg}
+          alt="제품 이미지"
+          style={{ width: "302px", height: "302px" }}
+        />
+      </div>
+    </div>
   );
 }
