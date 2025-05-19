c7d4dc4d1c2b5c540907123771cafd589b1f3e4e
김채림 <woqja0905@naver.com>
2025-05-19 13:41:41 +0900

    update : mock data로 작업중

--- src/pages/home/MallSectionBox.tsx
+++ src/pages/home/MallSectionBox.tsx
@@ -8,14 +8,20 @@ export default function MallSectionBox() {
         height: "302px",
         gap: "0 24px",
         display: "flex",
+        marginTop: "30px",
       }}
     >
-      <div>
+      <div style={{ lineHeight: "24px" }}>
         <img
           src={ProductImg}
           alt="제품 이미지"
           style={{ width: "302px", height: "302px" }}
         />
+        <span style={{ fontSize: "14px", fontWeight: "bold" }}>CJ제일제당</span>
+        <span style={{ fontSize: "14px" }}>
+          브랜드 FESTA! 조미료/소스 ~ 1+1
+        </span>
+        <p style={{ fontWeight: "bold", lineHeight: "24px" }}>13,200원~</p>
       </div>
     </div>
   );
