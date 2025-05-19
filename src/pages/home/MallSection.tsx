b77586311cedb30e59d53b6a9170fc68410e90b9
김채림 <woqja0905@naver.com>
2025-05-19 13:21:35 +0900

    update : css 수정

--- src/pages/home/MallSection.tsx
+++ src/pages/home/MallSection.tsx
@@ -2,6 +2,15 @@ import MoreViewButton from "../../components/common/MoreViewButton";
 import Tap from "../../components/common/Tap";
 import SectionTitle from "./SectionTitle";
 
+import styled from "styled-components";
+
+const MallSectionWrapper = styled.div`
+  width: 100%;
+  height: 300px;
+  margin-top: 120px;
+  border: 1px solid red;
+`;
+
 const tabsData = [
   { id: "banner_1", label: "오반장" },
   { id: "banner_2", label: "1+1" },
@@ -12,17 +21,10 @@ const tabsData = [
 
 export default function MallSection() {
   return (
-    <div
-      style={{
-        width: "100%",
-        height: "300px",
-        marginTop: "120px",
-        border: "1px solid red",
-      }}
-    >
+    <MallSectionWrapper style={{}}>
       <SectionTitle title="지금 세일 중인 상품이에요" />
       <Tap tabs={tabsData} />
       <MoreViewButton path="/a" title="오반장 전체보기" />
-    </div>
+    </MallSectionWrapper>
   );
 }
