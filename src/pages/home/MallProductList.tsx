9809093b6144869d8934cbfbbcc9ba07e25b0b16
김채림 <woqja0905@naver.com>
2025-05-19 14:19:27 +0900

    update : styled-component로 변경

--- src/pages/home/MallProductList.tsx
+++ src/pages/home/MallProductList.tsx
@@ -3,41 +3,60 @@ import ProductImg from "../../assets/product_1.avif";
 
 import DeliveryMark from "../../components/common/DeliveryMark";
 
-export default function MallSectionBox() {
+import styled from "styled-components";
+
+const MallProductListWrapper = styled.div`
+  gap: 0 24px;
+  display: flex;
+  flex-wrap: wrap;
+  margin-top: 30px;
+`;
+
+const ProductListLink = styled.a`
+  & div {
+    line-height: 24px;
+  }
+`;
+
+const ProductListImg = styled.img`
+  width: 302px;
+  height: 302px;
+`;
+
+const ProductComponyName = styled.span`
+  font-size: 14px;
+  font-weight: bold;
+`;
+
+const ProductName = styled.span`
+  font-size: 14px;
+`;
+
+const ProductPrice = styled.p`
+  font-weight: bold;
+  line-height: 24px;
+`;
+
+export default function MallProductList() {
   return (
-    <div
-      style={{
-        width: "302px",
-        height: "302px",
-        gap: "0 24px",
-        display: "flex",
-        marginTop: "30px",
-      }}
-    >
-      <a href="#">
-        {" "}
+    <MallProductListWrapper>
+      <ProductListLink href="#">
         {/* href는 값이 달라짐 */}
-        <div style={{ lineHeight: "24px" }}>
-          <img
-            src={ProductImg}
-            alt="제품 이미지"
-            style={{ width: "302px", height: "302px" }}
-          />
+        <ProductPrice>
+          <ProductListImg src={ProductImg} alt="제품 이미지" />
           {/* src,alt 는 값이 달라짐 */}
           <DeliveryMark src={dawnDeliveryMark} alt="새벽배송마크" />
           {/* mark는 있을수도 있고 없을 수도 있음 */}
-          <span style={{ fontSize: "14px", fontWeight: "bold" }}>
-            CJ제일제당
-          </span>
+          <ProductComponyName>CJ제일제당</ProductComponyName>
           {/* span 는 값이 달라짐 */}
-          <span style={{ fontSize: "14px" }}>
-            브랜드 FESTA! 조미료/소스 ~ 1+1
-          </span>
+          <ProductName>브랜드 FESTA! 조미료/소스 ~ 1+1</ProductName>
           {/* span 는 값이 달라짐 */}
-          <p style={{ fontWeight: "bold", lineHeight: "24px" }}>13,200원~</p>
+          <ProductPrice style={{ fontWeight: "bold", lineHeight: "24px" }}>
+            13,200원~
+          </ProductPrice>
           {/* p 는 값이 달라짐 */}
-        </div>
-      </a>
-    </div>
+        </ProductPrice>
+      </ProductListLink>
+    </MallProductListWrapper>
   );
 }
