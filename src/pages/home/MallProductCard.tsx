631bbc42d46884f0e05013762deebbe999947438
김채림 <woqja0905@naver.com>
2025-05-19 14:42:52 +0900

    update : props 설정, map 설정 코드

---
+++ src/pages/home/MallProductCard.tsx
@@ -0,0 +1,62 @@
+import DeliveryMark from "../../components/common/DeliveryMark";
+
+import styled from "styled-components";
+
+const ProductListLink = styled.a`
+  & div {
+    line-height: 24px;
+  }
+`;
+
+const ProductListImg = styled.img`
+  width: 302px;
+  height: 302px;
+`;
+
+const ProductComponyName = styled.span`
+  font-size: 14px;
+  font-weight: bold;
+`;
+
+const ProductName = styled.span`
+  font-size: 14px;
+`;
+
+const ProductPrice = styled.p`
+  font-weight: bold;
+  line-height: 24px;
+`;
+
+export interface ProductCardProps {
+  href: string;
+  imgSrc: string;
+  imgAlt: string;
+  markSrc?: string;
+  markAlt?: string;
+  componyName: string;
+  productName: string;
+  productPrice: string;
+}
+
+export default function MallProductCard({
+  href,
+  imgSrc,
+  imgAlt,
+  markSrc,
+  markAlt,
+  componyName,
+  productName,
+  productPrice,
+}: ProductCardProps) {
+  return (
+    <ProductListLink href={href}>
+      <div>
+        <ProductListImg src={imgSrc} alt={imgAlt} />
+        {markSrc && <DeliveryMark src={markSrc} alt={markAlt ?? "배송마크"} />}
+        <ProductComponyName>{componyName}</ProductComponyName>
+        <ProductName>{productName}</ProductName>
+        <ProductPrice>{productPrice}</ProductPrice>
+      </div>
+    </ProductListLink>
+  );
+}
--- src/pages/home/MallProductList.tsx
+++ src/pages/home/MallProductList.tsx
@@ -1,7 +1,4 @@
-import dawnDeliveryMark from "../../assets/dawnDeliveryMark.avif";
-import ProductImg from "../../assets/product_1.avif";
-
-import DeliveryMark from "../../components/common/DeliveryMark";
+import MallProductCard, { type ProductCardProps } from "./MallProductCard";
 
 import styled from "styled-components";
 
@@ -12,51 +9,16 @@ const MallProductListWrapper = styled.div`
   margin-top: 30px;
 `;
 
-const ProductListLink = styled.a`
-  & div {
-    line-height: 24px;
-  }
-`;
+interface MallProductListProps {
+  products: ProductCardProps[];
+}
 
-const ProductListImg = styled.img`
-  width: 302px;
-  height: 302px;
-`;
-
-const ProductComponyName = styled.span`
-  font-size: 14px;
-  font-weight: bold;
-`;
-
-const ProductName = styled.span`
-  font-size: 14px;
-`;
-
-const ProductPrice = styled.p`
-  font-weight: bold;
-  line-height: 24px;
-`;
-
-export default function MallProductList() {
+export default function MallProductList({ products }: MallProductListProps) {
   return (
     <MallProductListWrapper>
-      <ProductListLink href="#">
-        {/* href는 값이 달라짐 */}
-        <ProductPrice>
-          <ProductListImg src={ProductImg} alt="제품 이미지" />
-          {/* src,alt 는 값이 달라짐 */}
-          <DeliveryMark src={dawnDeliveryMark} alt="새벽배송마크" />
-          {/* mark는 있을수도 있고 없을 수도 있음 */}
-          <ProductComponyName>CJ제일제당</ProductComponyName>
-          {/* span 는 값이 달라짐 */}
-          <ProductName>브랜드 FESTA! 조미료/소스 ~ 1+1</ProductName>
-          {/* span 는 값이 달라짐 */}
-          <ProductPrice style={{ fontWeight: "bold", lineHeight: "24px" }}>
-            13,200원~
-          </ProductPrice>
-          {/* p 는 값이 달라짐 */}
-        </ProductPrice>
-      </ProductListLink>
+      {products.map((product, index) => (
+        <MallProductCard key={index} {...product} />
+      ))}
     </MallProductListWrapper>
   );
 }
