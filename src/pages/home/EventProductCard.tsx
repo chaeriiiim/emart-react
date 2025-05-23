15766f7f0ca92959c8ce59acd7576767fde29104
김채림 <woqja0905@naver.com>
2025-05-23 17:45:11 +0900

    update : test 코드

---
+++ src/pages/home/EventProductCard.tsx
@@ -0,0 +1,9 @@
+export default function EventProdcutCard() {
+  return (
+    <div style={{ width: "100%", backgroundColor: "orange" }}>
+      <div
+        style={{ width: "1280px", height: "500px", border: "1px solid red" }}
+      ></div>
+    </div>
+  );
+}
