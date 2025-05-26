998e9040c12d4ce2e1010cd32b3a81507da05148
김채림 <woqja0905@naver.com>
2025-05-26 17:59:35 +0900

    update : 기본 레이아웃 완성

--- src/pages/home/EventProductCard.tsx
+++ src/pages/home/EventProductCard.tsx
@@ -1,3 +1,5 @@
+import test from "../../assets/assay_product_1.avif";
+
 export default function EventProdcutCard() {
   return (
     <div
@@ -8,14 +10,22 @@ export default function EventProdcutCard() {
         margin: "0 auto",
       }}
     >
-      <div
-        style={{
-          width: "400px",
-          height: "240px",
-          backgroundColor: "blue",
-          borderRadius: "12px",
-        }}
-      ></div>
+      <div>
+        <a href="#">
+          <img
+            style={{ width: "400px", height: "240px", borderRadius: "12px" }}
+            src={test}
+          />
+          <div style={{ marginTop: "20px" }}>
+            <p style={{ fontSize: "20px", fontWeight: "bold" }}>
+              미식관 PICK: 문경사과
+            </p>
+            <p style={{ fontSize: "12px", marginTop: "4px" }}>
+              풍부한 과즙, 싱그러운 달콤함
+            </p>
+          </div>
+        </a>
+      </div>
     </div>
   );
 }
