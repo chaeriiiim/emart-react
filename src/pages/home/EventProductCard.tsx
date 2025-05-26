86ec8e11ef708bc087f204c72846b71410d1befe
김채림 <woqja0905@naver.com>
2025-05-26 22:12:57 +0900

    update : styled-component 작업

--- src/pages/home/EventProductCard.tsx
+++ src/pages/home/EventProductCard.tsx
@@ -1,31 +1,53 @@
 import test from "../../assets/assay_product_1.avif";
+import MoreViewButton from "../../components/common/MoreViewButton";
+
+import styled from "styled-components";
+
+const EventProductCardWrapper = styled.div`
+  width: 1280px;
+  // height: 500px;
+  margin: 0 auto;
+`;
+
+const EventCardContent = styled.div`
+  & img {
+    width: 400px;
+    height: 240px;
+    border-radius: 12p;
+  }
+`;
+
+const TItleWrapper = styled.div`
+  margin-top: 20px;
+`;
+
+const EventTitle = styled.div`
+  font-size: 20px;
+  font-weight: bold;
+`;
+
+const EventSubTitle = styled.div`
+  font-size: 12px;
+  margin-top: 4px;
+`;
 
 export default function EventProdcutCard() {
   return (
-    <div
-      style={{
-        width: "1280px",
-        height: "500px",
-        border: "1px solid red",
-        margin: "0 auto",
-      }}
-    >
-      <div>
+    <EventProductCardWrapper>
+      <EventCardContent>
         <a href="#">
-          <img
-            style={{ width: "400px", height: "240px", borderRadius: "12px" }}
-            src={test}
-          />
-          <div style={{ marginTop: "20px" }}>
-            <p style={{ fontSize: "20px", fontWeight: "bold" }}>
-              미식관 PICK: 문경사과
-            </p>
-            <p style={{ fontSize: "12px", marginTop: "4px" }}>
-              풍부한 과즙, 싱그러운 달콤함
-            </p>
-          </div>
+          {/* href props 시 다른 값 */}
+          <img src={test} alt="사과 이벤트" />
+          {/* src,alt props 시 다른 값 */}
+          <TItleWrapper>
+            <EventTitle style={{}}>미식관 PICK: 문경사과</EventTitle>
+            {/* title props 시 다른 값 */}
+            <EventSubTitle>풍부한 과즙, 싱그러운 달콤함</EventSubTitle>
+            {/* subtitle props 시 다른 값 */}
+          </TItleWrapper>
         </a>
-      </div>
-    </div>
+      </EventCardContent>
+      <MoreViewButton title="기획전 전체보기" path="#" />
+    </EventProductCardWrapper>
   );
 }
