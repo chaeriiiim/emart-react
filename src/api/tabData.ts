e4b1288491c24c3048d586d92920a3d70784478e
김채림 <woqja0905@naver.com>
2025-05-27 17:20:04 +0900

    update : tabData 작성

---
+++ src/api/tabData.ts
@@ -0,0 +1,14 @@
+export const saleTabs = [
+  { id: "sale_1", label: "오반장" },
+  { id: "sale_2", label: "1+1" },
+  { id: "sale_3", label: "마감세일" },
+  { id: "sale_4", label: "이마트 전단행사" },
+  { id: "sale_5", label: "트레이더스 점포행사" },
+];
+
+export const brandTabs = [
+  { id: "brand_1", label: "노브랜드" },
+  { id: "brand_2", label: "피코크" },
+  { id: "brand_3", label: "자주" },
+  { id: "brand_4", label: "건강식품" },
+];
