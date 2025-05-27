45fb640f23785dcfee33eba2075f226dff5fd9b0
김채림 <woqja0905@naver.com>
2025-05-27 20:10:22 +0900

    update : tab data 수정

--- src/api/tabData.ts
+++ src/api/tabData.ts
@@ -1,14 +1,39 @@
+// export const saleTabs = [
+//   { id: "sale_1", label: "오반장" },
+//   { id: "sale_2", label: "1+1" },
+//   { id: "sale_3", label: "마감세일" },
+//   { id: "sale_4", label: "이마트 전단행사" },
+//   { id: "sale_5", label: "트레이더스 점포행사" },
+// ];
+
+// export const brandTabs = [
+//   { id: "brand_1", label: "노브랜드" },
+//   { id: "brand_2", label: "피코크" },
+//   { id: "brand_3", label: "자주" },
+//   { id: "brand_4", label: "건강식품" },
+// ];
+
+// mockData.ts
 export const saleTabs = [
-  { id: "sale_1", label: "오반장" },
-  { id: "sale_2", label: "1+1" },
-  { id: "sale_3", label: "마감세일" },
-  { id: "sale_4", label: "이마트 전단행사" },
-  { id: "sale_5", label: "트레이더스 점포행사" },
+  { id: "banner_1", label: "오반장", buttonTitle: "오반장 전체보기" },
+  { id: "banner_2", label: "1+1", buttonTitle: "1+1 전체보기" },
+  { id: "banner_3", label: "마감세일", buttonTitle: "마감세일 전체보기" },
+  {
+    id: "banner_4",
+    label: "이마트 전단행사",
+    buttonTitle: "전단행사 전체보기",
+  },
+  {
+    id: "banner_5",
+    label: "트레이더스 점포행사",
+    buttonTitle: "트레이더스 전체보기",
+  },
 ];
 
-export const brandTabs = [
-  { id: "brand_1", label: "노브랜드" },
-  { id: "brand_2", label: "피코크" },
-  { id: "brand_3", label: "자주" },
-  { id: "brand_4", label: "건강식품" },
-];
+export const saleProductsMap = {
+  banner_1: [],
+  banner_2: [],
+  banner_3: [],
+  banner_4: [],
+  banner_5: [],
+};
