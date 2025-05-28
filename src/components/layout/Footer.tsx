3d1af3dfdf08d51c97f0924dd4dd450b2da13950
김채림 <woqja0905@naver.com>
2025-05-28 14:06:45 +0900

    update : footer 레이아웃

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,7 +1,68 @@
+import styled from "styled-components";
+
+const FooterWrapper = styled.div`
+  width: 100%;
+`;
+
+const FooterMainNavWrapper = styled.div`
+  width: 100%;
+  border-bottom: 1px solid #e4e4e4;
+  height: 51px;
+`;
+
+const FooterMainNavInner = styled.div`
+  width: 1280px;
+  height: 100%;
+  margin: 0 auto;
+  display: flex;
+  align-items: center;
+`;
+
+const FooterMainNavLink = styled.a`
+  font-size: 13px;
+  color: #777777;
+  font-weight: bold;
+  margin: 0 20px 0 0;
+
+  &:not(:last-child)::after {
+    content:"|";
+    display:"inline-block;
+    right:0;
+    color: #e5e5e5;
+    height:12px;
+    margin-left:20px;
+    width:1px;
+  }
+`;
+
+interface FooterMainNavItem {
+  href: string;
+  title: string;
+}
+
+const footerMainNav: FooterMainNavItem[] = [
+  { href: "#", title: "회사소개" },
+  { href: "#", title: "개인정보처리방침" },
+  { href: "#", title: "이용약관" },
+  { href: "#", title: "청소년 보호방침" },
+  { href: "#", title: "지식재산권센터" },
+  { href: "#", title: "입점상담" },
+  { href: "#", title: "이마트점포 입점상담" },
+  { href: "#", title: "광고신청" },
+];
+
 export default function Footer() {
   return (
-    <>
-      <div>test!</div>
-    </>
+    <FooterWrapper>
+      <FooterMainNavWrapper>
+        <FooterMainNavInner>
+          {footerMainNav.map((item, index) => (
+            <FooterMainNavLink key={index} href={item.href}>
+              {item.title}
+            </FooterMainNavLink>
+          ))}
+        </FooterMainNavInner>
+      </FooterMainNavWrapper>
+    </FooterWrapper>
   );
 }
