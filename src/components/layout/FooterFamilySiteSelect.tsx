dc0eadd42bcff527abcc03c0679e56fe2a88cdec
김채림 <woqja0905@naver.com>
2025-06-04 11:34:24 +0900

    update : add img

--- src/components/layout/FooterFamilySiteSelect.tsx
+++ src/components/layout/FooterFamilySiteSelect.tsx
@@ -1,22 +1,41 @@
-// import { useState } from "react";
+import { useState } from "react";
 // import { familySiteList } from "../../api/familySiteList";
 
 // import styled from "styled-components";
 
 export default function FooterFamilySiteSelect() {
-  // const [opne, setOpen] = useState(false);
+  const [opne, setOpen] = useState(false);
 
-  // const toggleDropdown = () => setOpen((prev) => !prev);
+  const toggleDropdown = () => setOpen((prev) => !prev);
 
   return (
     <div
       style={{
         position: "relative",
-        width: "200px",
+        width: "182px",
+        height: "30px",
         border: "1px solid #ccc",
         textAlign: "left",
         backgroundColor: "#fff",
+        padding: "0px 10px 0px 15px",
+        display: "flex",
+        alignItems: "center",
+        color: "#444",
       }}
-    ></div>
+    >
+      <div
+        style={{
+          // padding: "12px 16px",
+          cursor: "pointer",
+          display: "flex",
+          justifyContent: "space-between",
+
+          fontSize: "12px",
+        }}
+        onClick={toggleDropdown}
+      >
+        FAMILY SITE
+      </div>
+    </div>
   );
 }
