b83cf69a821d7f8f91b0f029e6073abd50bc8cec
김채림 <woqja0905@naver.com>
2025-06-12 15:09:55 +0900

    update : 주석

--- src/components/layout/FooterFamilySiteSelect.tsx
+++ src/components/layout/FooterFamilySiteSelect.tsx
@@ -72,6 +72,9 @@ const DropdownItem = styled.li`
 `;
 
 export default function FooterFamilySiteSelect() {
+  {
+    /* footer 사이트 select box */
+  }
   const [open, setOpen] = useState(false);
 
   const toggleDropdown = () => setOpen((prev) => !prev);
