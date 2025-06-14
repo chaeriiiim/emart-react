ece2528c765c616967e4ccb1245544b094f47748
김채림 <woqja0905@naver.com>
2025-06-14 15:45:08 +0900

    update : info 작성중

--- src/components/layout/FooterComInfo.tsx
+++ src/components/layout/FooterComInfo.tsx
@@ -5,8 +5,6 @@ export default function FooterComInfo() {
     <div
       style={{
         width: "1280px",
-        height: "100px",
-        border: "1px solid red",
         margin: "0 auto",
       }}
     >
@@ -18,6 +16,19 @@ export default function FooterComInfo() {
         <LinkButton href="#" title="전화문의 전 클릭" />
         <LinkButton href="#" title="1:1 고객센터톡" />
         <p style={{ fontSize: "12px" }}>고객센터/전자금융거래분쟁처리</p>
+      </div>
+      <div>
+        <p
+          style={{
+            fontSize: "12px",
+            lineHeight: "23px",
+            color: "#777",
+            gap: "7px",
+          }}
+        >
+          <span>대표자:최훈학</span>
+          <span>서울특별시 영등포구 영신로 34길 30</span>
+        </p>
       </div>
     </div>
   );
